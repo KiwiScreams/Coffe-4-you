3612f1bc3e9b6bd6b8066b9c71d86d5178565a42
KiwiScreams <screamskiwi3@gmail.com>
2024-08-06 13:16:54 +0400

    random citate

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,41 @@
 import "./Main.css";
+import { useState, useEffect } from "react";
 const Main = () => {
+  const citations = [
+    {
+      id: 1,
+      author: "John Doe",
+      citate: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      id: 2,
+      author: "Jane Doe",
+      citate:
+        "Quos provident fugiat, eum assumenda incidunt corporis doloribus veritatis.",
+    },
+    {
+      id: 3,
+      author: "Bob Smith",
+      citate:
+        "Temporibus, explicabo. Provident fugiat, eum assumenda incidunt corporis.",
+    },
+  ];
+  const [randomCitation, setRandomCitation] = useState({});
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * citations.length);
+    setRandomCitation(citations[randomIndex]);
+  }, []);
+
   return (
     <>
       <section className="main-container">
         <div className="text-container">
-          <h1>Coffe 4 You</h1>
+          <h1>Coffee 4 You</h1>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            provident fugiat, eum assumenda incidunt corporis doloribus
-            veritatis quibusdam hic officiis, ipsam ab deserunt recusandae
-            voluptate molestiae impedit animi. Temporibus, explicabo.
+            {randomCitation.citate}
           </p>
+          <p>{randomCitation.author}</p>
           <div className="button-container">
             <div className="line"></div>
             <button>View Our Menu</button>
