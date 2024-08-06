5a2b60960c3c2c517438e3b89193b5c9a808fa12
KiwiScreams <screamskiwi3@gmail.com>
2024-08-06 23:47:06 +0400

    menu list

--- src/assets/data/data.jsx
+++ src/assets/data/data.jsx
@@ -1,27 +1,45 @@
+import dessert1 from "../../assets/images/dessert1.png"
+import dessert2 from "../../assets/images/dessert2.png"
+import dessert3 from "../../assets/images/dessert3.png"
+import dessert4 from "../../assets/images/dessert4.png"
+import dessert5 from "../../assets/images/dessert5.png"
+import dessert6 from "../../assets/images/dessert6.png"
+import dessert7 from "../../assets/images/dessert7.png"
+import dessert8 from "../../assets/images/dessert8.png"
+import dessert9 from "../../assets/images/dessert9.png"
+
 export const deserts = [
   {
     id: 1,
     name: "Cheescake",
-    // image: car1,
+    image: dessert1,
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
     data: "data-1",
   },
   {
     id: 2,
-    name: "tortik",
-    // image: car1,
+    name: "Cheescake",
+    image: dessert2,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a ",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
     data: "data-2",
   },
   {
     id: 3,
-    name: "Toyota Crown 2023",
-    // image: car1,
+    name: "Cheescake",
+    image: dessert3,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unkn",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
     data: "data-3",
+  },
+  {
+    id: 4,
+    name: "Cheescake",
+    image: dessert4,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
+    data: "data-4",
   },
 ];
 export const coffee = [
--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,14 +1,17 @@
 import { deserts } from "../../assets/data/data";
 import MenuItem from "../../components/menu/menu-item/MenuItem";
-
+import "./Menu.css";
 const Menu = () => {
   return (
     <>
-      <div className="cars-container">
-        {deserts.map((data) => (
-          <MenuItem key={data.id} data={data} />
-        ))}
-      </div>
+      <section className="menu-section">
+        <h3>Desserts</h3>
+        <div className="menu-container">
+          {deserts.map((data) => (
+            <MenuItem key={data.id} data={data} />
+          ))}
+        </div>
+      </section>
     </>
   );
 };
