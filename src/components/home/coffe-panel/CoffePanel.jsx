c66abc5d22d1ec0010754afee7204b6c1ab86d35
KiwiScreams <screamskiwi3@gmail.com>
2024-08-06 15:43:41 +0400

    fix

--- src/components/home/coffe-panel/CoffePanel.jsx
+++ src/components/home/coffe-panel/CoffePanel.jsx
@@ -4,8 +4,8 @@ const CoffePanel = () => {
         <>
         <section className="coffe-panel-section">
             <div className="coffe-panel">
-                <h4 className="hover">coffe 4 you</h4>
-                <h4 className="text">coffe 4 you</h4>
+                <h4 className="hover">coffee 4 you</h4>
+                <h4 className="text">coffee 4 you</h4>
             </div>
         </section>
         </>
