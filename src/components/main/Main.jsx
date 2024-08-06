1d46ffdd8567e01cb6f9b2d6bd827df8e8ddabaa
KiwiScreams <screamskiwi3@gmail.com>
2024-08-06 13:40:41 +0400

    citations

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,24 +4,68 @@ const Main = () => {
   const citations = [
     {
       id: 1,
-      author: "John Doe",
-      citate: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      author: "",
+      citate: "Coffee is a daily necessity",
     },
     {
       id: 2,
-      author: "Jane Doe",
-      citate:
-        "Quos provident fugiat, eum assumenda incidunt corporis doloribus veritatis.",
+      author: "",
+      citate: "I like my coffee like I like my mornings: dark and strong.",
     },
     {
       id: 3,
-      author: "Bob Smith",
+      author: "",
       citate:
-        "Temporibus, explicabo. Provident fugiat, eum assumenda incidunt corporis.",
+        "A cup of coffee shared with a friend is happiness tasted and time well spent.",
+    },
+    {
+      id: 4,
+      author: "",
+      citate: "Coffee is a magic potion that helps us face the daily grind.",
+    },
+    {
+      id: 5,
+      author: "",
+      citate: "Coffee first. Schemes later.",
+    },
+    {
+      id: 6,
+      author: "",
+      citate:
+        "Good ideas start with brainstorming. Great ideas start with coffee.",
+    },
+    {
+      id: 7,
+      author: "Terri Guillemets",
+      citate: "Coffee is the best thing to douse the sunrise with.",
+    },
+    {
+      id: 8,
+      author: "Jackie Chan",
+      citate: "Coffee is a language in itself.",
+    },
+    {
+      id: 9,
+      author: "",
+      citate: "Life happens, coffee helps.",
+    },
+    {
+      id: 10,
+      author: "Louisa May Alcott, Little Women",
+      citate: "I'd rather take coffee than compliments just now.",
+    },
+    {
+      id: 11,
+      author: "Terry Pratchett",
+      citate: "Coffee is a way of stealing time that should by rights belong to your older self.",
+    },
+    {
+      id: 12,
+      author: "Hugh Jackman",
+      citate: "To me, the smell of fresh-made coffee is one of the greatest inventions.",
     },
   ];
   const [randomCitation, setRandomCitation] = useState({});
-
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * citations.length);
     setRandomCitation(citations[randomIndex]);
@@ -32,10 +76,8 @@ const Main = () => {
       <section className="main-container">
         <div className="text-container">
           <h1>Coffee 4 You</h1>
-          <p>
-            {randomCitation.citate}
-          </p>
-          <p>{randomCitation.author}</p>
+          <p>"{randomCitation.citate}"</p>
+          <cite>{randomCitation.author}</cite>
           <div className="button-container">
             <div className="line"></div>
             <button>View Our Menu</button>
