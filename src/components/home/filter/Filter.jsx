ec61c34be003747a37b528f1ad915942450486fd
KiwiScreams <screamskiwi3@gmail.com>
2024-08-06 17:00:06 +0400

    image animation

--- src/components/home/filter/Filter.jsx
+++ src/components/home/filter/Filter.jsx
@@ -1,41 +1,30 @@
 import { NavLink } from "react-router-dom";
 import "./Filter.css";
 import coffee from "../../../assets/images/coffe-filter.png";
-import coffeeHover from "../../../assets/images/coffe-logo.png";
 import cake from "../../../assets/images/cake-filter.png";
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 import masterpiece from "../../../assets/images/masterpiece-filter.png";
 const Filter = () => {
-  const [isHovered, setIsHovered] = useState({
-    coffee: false,
-    cake: false,
-    masterpiece: false,
-  });
-
-  const handleMouseOver = (type) => {
-    setIsHovered((prev) => ({ ...prev, [type]: true }));
-  };
-
-  const handleMouseOut = (type) => {
-    setIsHovered((prev) => ({ ...prev, [type]: false }));
-  };
   return (
     <>
       <section className="filter-container">
-      <NavLink
-        onMouseOver={() => handleMouseOver("coffee")}
-        onMouseOut={() => handleMouseOut("coffee")}
-      >
-        <img src={isHovered.coffee ? coffeeHover : coffee} />
-        Coffee
-      </NavLink>
         <NavLink>
-          <img src={cake} />
+          <div className="image">
+            <img src={coffee} />
+          </div>
+          Coffee
+        </NavLink>
+        <NavLink>
+          <div className="image">
+            <img src={cake} />
+          </div>
           Deserts
         </NavLink>
         <NavLink>
-          <img src={masterpiece} />
+          <div className="image">
+            <img src={masterpiece} />
+          </div>
           Masterpiece
         </NavLink>
       </section>
