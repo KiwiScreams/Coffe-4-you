45bf78dcae547848fe37c8a732e4b0108612eaf4
KiwiScreams <screamskiwi3@gmail.com>
2024-08-07 18:40:07 +0400

    desserts info

--- src/assets/data/data.jsx
+++ src/assets/data/data.jsx
@@ -11,35 +11,69 @@ import dessert9 from "../../assets/images/dessert9.png"
 export const deserts = [
   {
     id: 1,
-    name: "Cheescake",
+    name: "Classic Chocolate Cake",
     image: dessert1,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
-    data: "data-1",
+      "A rich and moist chocolate cake layered with velvety chocolate ganache and topped with chocolate shavings.",
+    ingridients: "Flour, cocoa powder, sugar, eggs, butter, milk, baking powder, salt, vanilla extract, heavy cream, chocolate.",
+    price: 25,
+    data: "classic-chocolate-cake-1",
+    history: "Chocolate cake has been enjoyed since the 19th century. The introduction of cocoa powder by Dutch chemist Coenraad Johannes van Houten in the 1820s made chocolate cakes more accessible and widespread. The classic recipe evolved from simple chocolate-flavored cakes to rich, layered cakes thanks to advancements in baking technology and ingredients."
   },
   {
     id: 2,
-    name: "Cheescake",
+    name: "Lemon Blueberry Cake",
     image: dessert2,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
-    data: "data-2",
+      "A light and zesty lemon-flavored cake with fresh blueberries and a tangy lemon glaze.",
+    ingridients: "Flour, sugar, eggs, butter, milk, lemon zest, lemon juice, blueberries, baking powder, salt.",
+    price: 28,
+    data: "lemon-blueberry-cake-2",
+    history: "History: Lemon and blueberry flavors have been popular in American baking since the early 20th century. Lemon cakes have their roots in European baking traditions, with lemon being a common ingredient in English and French desserts. Blueberries, native to North America, have been paired with lemon in desserts since the late 19th century, particularly in New England."
   },
   {
     id: 3,
-    name: "Cheescake",
+    name: "Red Velvet Cake",
     image: dessert3,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
-    data: "data-3",
+      "A classic red velvet cake with a hint of cocoa and a smooth cream cheese frosting.",
+    ingridients: "Flour, cocoa powder, sugar, eggs, butter, buttermilk, red food coloring, baking soda, vinegar, salt, cream cheese, powdered sugar.",
+    price: 30,
+    data: "red-velvet-cake-3",
+    history: "Red velvet cake became popular in the 1920s and 1930s, with its distinctive red color attributed to the reaction between cocoa powder and acidic buttermilk. The cake’s origins are debated, but it gained widespread fame after the Waldorf-Astoria Hotel in New York City began serving it. Its popularity surged again in the 2000s thanks to the rise of food blogs and TV shows."
   },
   {
     id: 4,
-    name: "Cheescake",
+    name: "Carrot Cake",
     image: dessert4,
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and ",
-    data: "data-4",
+      "A moist and spiced carrot cake loaded with walnuts and topped with a creamy cream cheese frosting.",
+    ingridients: "Flour, sugar, carrots, eggs, oil, walnuts, baking powder, baking soda, cinnamon, nutmeg, salt, cream cheese, powdered sugar.",
+    price: 27,
+    data: "carrot-cake-4",
+    history: "Carrot cake has its origins in medieval Europe, where carrots were used as a sweetener in cakes due to their natural sugars. The modern version, with spices like cinnamon and nutmeg, became popular in the United States during the 1960s and 1970s, partly due to the health food movement. Carrot cake's rich and moist texture was a hit in the era of more wholesome, natural ingredients."
+  },
+  {
+    id: 5,
+    name: "Tiramisu",
+    image: dessert5,
+    description:
+      "A classic Italian dessert made with layers of coffee-soaked ladyfingers and mascarpone cream, dusted with cocoa powder.",
+    ingridients: "Ladyfingers, mascarpone cheese, eggs, sugar, coffee, cocoa powder, vanilla extract.",
+    price: 32,
+    data: "tiramisu-5",
+    history: "Tiramisu is a relatively modern Italian dessert, believed to have originated in the Veneto region of Italy in the 1960s. The name translates to 'pick me up,' referring to the energizing effects of coffee. The dessert combines layers of coffee-soaked ladyfingers with mascarpone cheese and cocoa powder, reflecting Italian culinary traditions of layering and simplicity."
+  },
+  {
+    id: 6,
+    name: "Vanilla Bean Cheesecake",
+    image: dessert6,
+    description:
+      "A creamy cheesecake infused with real vanilla bean and a buttery graham cracker crust.",
+    ingridients: "Cream cheese, sugar, eggs, vanilla beans, sour cream, heavy cream, graham crackers, butter.",
+    price: 35,
+    data: "vanilla-bean-cheesecake-6",
+    history: "Cheesecake has ancient origins, with early versions found in Greek and Roman cuisine. The modern vanilla bean cheesecake as we know it evolved in the United States during the 19th and 20th centuries. The introduction of cream cheese by William Lawrence in 1872 played a significant role in shaping the creamy texture of the modern cheesecake. Vanilla beans have been used since the 16th century, adding a rich, aromatic flavor to the dessert."
   },
 ];
 export const coffee = [
