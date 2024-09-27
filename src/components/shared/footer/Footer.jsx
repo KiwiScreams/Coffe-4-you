c8c051af77870f593db971571505cbe626ab3a81
KiwiScreams <screamskiwi3@gmail.com>
2024-09-27 19:14:17 +0400

    delete imports

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import "./Footer.css";
 import footerImage from "../../../assets/images/footer-coffe.png";
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 const Footer = () => {
   const navigate = useNavigate();
